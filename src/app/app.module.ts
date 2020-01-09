2cc8cd1ff26b0d6f3a6dd7de103040d52afdef32
Hong Quan <hohongquan.bk@gmail.com>
2020-01-09 18:00:48 +0100

    remove unused packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxMatDatetimePickerModule, NgxMatTimepickerModule } from 'projects/ngx-mat-datetime-picker/src/public-api';
+import { AppComponent } from './app.component';
 
 @NgModule({
    declarations: [
@@ -17,7 +16,6 @@ import { NgxMatDatetimePickerModule, NgxMatTimepickerModule } from 'projects/ngx
    imports: [
       BrowserModule,
       HttpClientModule,
-      HttpModule,
       BrowserAnimationsModule,
       MatDatepickerModule,
       MatInputModule,
