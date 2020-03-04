c887ad89cc1865001cd3cbd00d7d997ec44f6143
Hong Quan <hohongquan.bk@gmail.com>
2020-03-04 17:32:35 +0100

    add date formats

--- projects/ngx-mat-datetime-picker/src/lib/core/index.ts
+++ projects/ngx-mat-datetime-picker/src/lib/core/index.ts
@@ -8,12 +8,14 @@
 
 import { PlatformModule } from '@angular/cdk/platform';
 import { NgModule } from '@angular/core';
-import { MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS } from '@angular/material/core';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
 import { NgxMatDateAdapter } from './ngx-mat-date-adapter';
 import { NgxMatNativeDateAdapter } from './ngx-mat-native-date-adapter';
+import { NGX_MAT_NATIVE_DATE_FORMATS } from './ngx-mat-native-date-formats';
 
 export * from './ngx-mat-date-adapter';
 export * from './ngx-mat-native-date-adapter';
+export * from './ngx-mat-native-date-formats';
 
 
 @NgModule({
@@ -26,6 +28,6 @@ export class NgxNativeDateModule { }
 
 @NgModule({
     imports: [NgxNativeDateModule],
-    providers: [{ provide: MAT_DATE_FORMATS, useValue: MAT_NATIVE_DATE_FORMATS }],
+    providers: [{ provide: MAT_DATE_FORMATS, useValue: NGX_MAT_NATIVE_DATE_FORMATS }],
 })
 export class NgxMatNativeDateModule { }
