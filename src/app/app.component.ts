e6522f1c026ba44b48bd99f44c1c941fb12426bc
Hong Quan <hohongquan.bk@gmail.com>
2020-03-16 19:43:57 +0100

    demo datepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
@@ -17,6 +18,9 @@ export class AppComponent implements OnInit {
   public disableSecond = true;
   public minDate: Date;
   public maxDate: Date;
+  public stepHour = 1;
+  public stepMinute = 1;
+  public stepSecond = 1;
 
   public formGroup = new FormGroup({
     date: new FormControl(null, [Validators.required])
@@ -24,20 +28,53 @@ export class AppComponent implements OnInit {
   public dateControl = new FormControl(new Date());
   public dateControlMinMax = new FormControl(new Date());
 
+  public options = [
+    { value: true, label: 'True' },
+    { value: false, label: 'False' }
+  ];
+
+  public stepHours = [1, 2, 3, 4, 5];
+  public stepMinutes = [1, 5, 10, 15, 20, 25];
+  public stepSeconds = [1, 5, 10, 15, 20, 25];
+
   constructor(private http: HttpClient, private zone: NgZone) {
-    this.minDate = new Date();
-    this.maxDate = new Date();
   }
 
   ngOnInit() {
     this.date = null;
-    const now = new Date();
-    this.minDate.setDate(now.getDate() - 1);
-    this.maxDate.setDate(now.getDate() + 1);
+  }
+
+  toggleMinDate(evt: any) {
+    if (evt.checked) {
+      this._setMinDate();
+    } else {
+      this.minDate = null;
+    }
+  }
+
+  toggleMaxDate(evt: any) {
+    if (evt.checked) {
+      this._setMaxDate();
+    } else {
+      this.maxDate = null;
+    }
   }
 
   closePicker() {
     this.picker.cancel();
+  }
+
+  private _setMinDate() {
+    const now = new Date();
+    this.minDate = new Date();
+    this.minDate.setDate(now.getDate() - 1);
+  }
+
+
+  private _setMaxDate() {
+    const now = new Date();
+    this.maxDate = new Date();
+    this.maxDate.setDate(now.getDate() + 1);
   }
 
 }
