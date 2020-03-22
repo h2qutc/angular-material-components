f045bfcf170dd6c7892bb505ea0f2ff0c9cc6214
Hong Quan <hohongquan.bk@gmail.com>
2020-03-22 23:45:40 +0100

    rename

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  public nameApp = 'ngx-mat-components';
+  public nameApp = 'angular-material-components';
 
 
 
