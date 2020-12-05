69cd790e03635e7d42f7f3ae5ee5e2db4303cf15
Hong Quan <hohongquan.bk@gmail.com>
2020-12-05 14:20:45 +0700

    fix: #138

--- src/app/demo-datetime/demo-datetime.module.ts
+++ src/app/demo-datetime/demo-datetime.module.ts
@@ -13,6 +13,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '../../../projects/datetime-picker/src/public-api';
 import { DemoDatetimeComponent } from './demo-datetime.component';
 import { SharedModule } from '../shared';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 const routes: Routes = [
   { path: '', component: DemoDatetimeComponent }
@@ -41,6 +42,9 @@ const routes: Routes = [
   ],
   declarations: [
     DemoDatetimeComponent
+  ],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }
   ]
 })
 export class DemoDatetimeModule { }
