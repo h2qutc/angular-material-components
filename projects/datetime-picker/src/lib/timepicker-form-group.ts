f981b675903f47cc7021f43f0a60752269792755
Mathew Berg <berg.mathew@gmail.com>
2022-06-16 23:26:36 -0400

    White space fixes

--- projects/datetime-picker/src/lib/timepicker-form-group.ts
+++ projects/datetime-picker/src/lib/timepicker-form-group.ts
@@ -1,7 +1,7 @@
 import { FormControl } from '@angular/forms';
 
 export interface TimepickerFormGroup {
-	hour: FormControl<string>;
-	minute: FormControl<string>;
-	second: FormControl<string>;
+  hour: FormControl<string>;
+  minute: FormControl<string>;
+  second: FormControl<string>;
 }
